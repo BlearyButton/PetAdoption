237cfa505ac6d847e3a1675c0fa0281d49ac6f64
BlearyButton <117636640+BlearyButton@users.noreply.github.com>
2022-11-16 11:02:04 +0100

    frontpage created, not responsive

---
+++ pages/index.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="overflow-hidden min-h-screen relative bg-gray-200">
+<div class="z-20 flex flex-row justify-between py-16 ml-64 relative">
+  <div class="flex flex-row font-poppins text-md text-mediumpurple font-extrabold">Adopt <p class="ml-1 text-mediumorange">Today</p></div>
+  <div class="flex flex-row font-poppins text-md text-darkblue font-bold mr-24 ">
+    <div class="mx-12">The quiz</div>
+    <div class="mx-12">Our pets</div>
+    <div class="mx-12">The process</div>
+    <div class="mx-12">Categories</div>
+    <div class="mx-12">Contact</div>
+  </div> 
+</div> 
+<div class="flex flex-row relative my-16 items-center">
+<div class="flex flex-col font-poppins">
+  <h3 class="mt-36 ml-64 text-mediumpurple font-extrabold">Adopt, don't shop</h3>
+  <div class="ml-64 my-4 text-6xl text-darkblue font-extrabold w-3/6 leading-normal">Give our animals a better home</div>
+  <div class="ml-64 w-48 text-center font-poppins text-md bg-darkorange text-white rounded-lg pl-6 pr-6 pt-3 pb-3">Do the quiz!</div>
+  <div class="mt-24 flex justify-start"> 
+      <div class="w-11/12 bg-darkpurple rounded-r-2xl px-12 py-6">
+        <div class="flex flex-row items-center">
+          <img src="../assets/images/adopt/footsteps1.png" class="w-1/12"/>
+          <div class="flex flex-col ml-48 mr-12 text-center ">
+            <div class="font-extrabold text-gray-200 font-poppins text-5xl ">2500+</div>
+            <div class="mt-4 font-medium text-gray-200 text-sm font-poppins">Adopted pets</div>
+          </div>
+          <div class="flex flex-col mx-12 text-center ">
+            <div class="font-extrabold text-gray-200 font-poppins text-5xl ">1700+</div>
+            <div class="mt-4 font-medium text-gray-200 text-sm font-poppins">Customers</div>
+          </div>
+          <div class="flex flex-col mx-12 text-center ">
+            <div class="font-extrabold text-gray-200 font-poppins text-5xl ">10</div>
+            <div class="mt-4 font-medium text-gray-200 text-sm font-poppins">Years experience</div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+<img src="../assets/images/adopt/blobcat.png" class="w-6/12 absolute -right-56 -top-72"/>
+</div>   
+    <img src="../assets/images/adopt/yellowround.png" class="absolute w-3/12 -left-72"/>
+    <img src="../assets/images/adopt/yellowround.png" class="absolute w-16 -right-8"/>
+    <div class="flex justify-center text-center mt-48 ">
+      <div class="mt-24 font-poppins">
+        <h3 class="text-mediumpurple font-extrabold">Our pets</h3>
+        <h1 class="mt-4 text-4xl text-darkblue font-extrabold">
+          Waiting for adoption
+        </h1>
+      </div>
+    </div>
+    <div class="mt-24 flex justify-center flex-row">
+      <div
+        class="mx-6 outline outline-2 pb-12 flex flex-col bg-white w-2/12 h-96 drop-shadow-xl rounded-lg"
+      >
+        <div
+          class="bg-mediumpurple basis-4/6 rounded-b-none rounded-t-lg flex justify-center items-center"
+        >
+          <img src="../assets/images/adopt/kitty.png" class="w-3/6 -mb-20" />
+        </div>
+        <div class="flex flex-col ml-8">
+          <div class="flex flex-row basis-2/6">
+            <div class="mt-10 flex flex-center">
+              <div class="font-poppins text-xl font-extrabold">Kitty</div>
+              <img src="../assets/images/adopt/iconmale.png" class="ml-2 h-4" />
+            </div>
+          </div>
+          <div class="font-poppins text-sm text-mediumgray font-extrabold">
+            6 months, 5 kg
+          </div>
+          <div class="mt-4 flex flex-row">
+            <div
+              class="mr-2 font-poppins text-sm bg-lightblue text-mediumblue rounded-lg pl-2 pr-2 pt-1 pb-1"
+            >
+              smart
+            </div>
+            <div
+              class="font-poppins text-sm bg-lightorange text-mediumorange rounded-lg pl-2 pr-2 pt-1 pb-1"
+            >
+              friendly
+            </div>
+          </div>
+          <div class="mt-4 flex flex-row items-center">
+            <div
+              class="font-extrabold text-xs font-poppins text-darkblue underline underline-offset-2"
+            >
+              Read more
+            </div>
+            <img
+              src="../assets/images/adopt/arrowrightdarkblue.png"
+              class="h-2 ml-1"
+            />
+          </div>
+        </div>
+      </div>
+      <div
+        class="mx-6 outline outline-2 pb-12 flex flex-col bg-white w-2/12 h-96 drop-shadow-xl rounded-lg"
+      >
+        <div
+          class="bg-mediumorange basis-4/6 rounded-b-none rounded-t-lg flex justify-center items-center"
+        >
+          <img src="../assets/images/adopt/bobby.png" class="w-4/6 -mb-32" />
+        </div>
+        <div class="flex flex-col ml-8">
+          <div class="flex flex-row basis-2/6">
+            <div class="mt-10 flex flex-center">
+              <div class="font-poppins text-xl font-extrabold">Bobby</div>
+              <img src="../assets/images/adopt/iconmale.png" class="ml-2 h-4" />
+            </div>
+          </div>
+          <div class="font-poppins text-sm text-mediumgray font-extrabold">
+            11 months, 13 kg
+          </div>
+          <div class="mt-4 flex flex-row">
+            <div
+              class="mr-2 font-poppins text-sm bg-lightorange text-mediumorange rounded-lg pl-2 pr-2 pt-1 pb-1"
+            >
+              kind
+            </div>
+            <div
+              class="mr-2 font-poppins text-sm bg-lightpurple text-mediumpurple rounded-lg pl-2 pr-2 pt-1 pb-1"
+            >
+              fun
+            </div>
+            <div
+              class="font-poppins text-sm bg-lightblue text-mediumblue rounded-lg pl-2 pr-2 pt-1 pb-1"
+            >
+              smart
+            </div>
+          </div>
+          <div class="mt-4 flex flex-row items-center">
+            <div
+              class="font-extrabold text-xs font-poppins text-darkblue underline underline-offset-2"
+            >
+              Read more
+            </div>
+            <img
+              src="../assets/images/adopt/arrowrightdarkblue.png"
+              class="h-2 ml-1"
+            />
+          </div>
+        </div>
+      </div>
+      <div
+        class="mx-6 outline outline-2 pb-12 flex flex-col bg-white w-2/12 h-96 drop-shadow-xl rounded-lg"
+      >
+        <div
+          class="bg-mediumblue basis-4/6 rounded-b-none rounded-t-lg flex justify-center items-center"
+        >
+          <img src="../assets/images/adopt/luccy.png" class="w-4/6 -mb-20" />
+        </div>
+        <div class="flex flex-col ml-8">
+          <div class="flex flex-row basis-2/6">
+            <div class="mt-10 flex flex-center items-center">
+              <div class="font-poppins text-xl font-extrabold">Luccy</div>
+              <img
+                src="../assets/images/adopt/iconfemale.png"
+                class="ml-2 h-4"
+              />
+            </div>
+          </div>
+          <div class="font-poppins text-sm text-mediumgray font-extrabold">
+            6 months, 5 kg
+          </div>
+          <div class="mt-4 flex flex-row">
+            <div
+              class="mr-2 font-poppins text-sm bg-lightorange text-mediumorange rounded-lg pl-2 pr-2 pt-1 pb-1"
+            >
+              kind
+            </div>
+            <div
+              class="font-poppins text-sm bg-lightblue text-mediumblue rounded-lg pl-2 pr-2 pt-1 pb-1"
+            >
+              smart
+            </div>
+          </div>
+          <div class="mt-4 flex flex-row items-center">
+            <div
+              class="font-extrabold text-xs font-poppins text-darkblue underline underline-offset-2"
+            >
+              Read more
+            </div>
+            <img
+              src="../assets/images/adopt/arrowrightdarkblue.png"
+              class="h-2 ml-1"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mt-24 flex justify-center flex-row">
+      <div class="w-2/12 text-center font-poppins text-md bg-darkorange text-white rounded-lg pl-6 pr-6 pt-3 pb-3">See all pets</div>
+      </div>
+    <div class="mt-72 flex justify-end"> 
+      <div class="w-4/6 bg-darkpurple rounded-l-2xl ">
+        <div class="flex flex-row justify-between items-center">
+          <div class="flex flex-col ml-32 ">
+            <div class="font-extrabold text-gray-200 font-poppins text-5xl ">Pick up your new friend</div>
+            <div class="mt-4 font-medium text-gray-200 text-sm font-poppins">Take the quiz to find out which pet will suit you best</div>
+          </div>
+          <img src="../assets/images/adopt/rabbithugs.png" class="mr-24 w-3/12 -mt-56 "/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'] 
+      },
+      colors: {
+        'darkblue': '#181F2F', 
+        'lightpurple': '#EDE9FF',
+        'mediumpurple': '#A99BED',
+        'darkpurple': '#5F4ACB',
+        'mediumgray': '#878B8F',
+        'lightblue': '#E5F0FD',
+        'mediumblue': '#73A9F5',
+        'lightorange': '#FDF2D8',
+        'mediumorange': '#F8B646',
+        'darkorange': '#F2B036',
+      },
+    },
+  },
+  variants: {
+  },
+  plugins: [
+  ],
+  safelist: [
+    {
+        pattern: /.*/,
+    },
+  ],
+}
